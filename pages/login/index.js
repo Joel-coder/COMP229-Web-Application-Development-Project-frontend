0056572a1d00a8e435f111b854d42d652635b9a4
Joel Vargas <joelvargasapolinario@gmail.com>
2022-01-14 16:53:59 -0500

    adding redirect to register

--- pages/login/index.js
+++ pages/login/index.js
@@ -3,10 +3,13 @@ import TextField from "@mui/material/TextField";
 import Container from "@mui/material/Container";
 import Button from "@mui/material/Button";
 import API from "../../api";
+import { FormHelperText } from "@mui/material";
 //require("typeface-poppins");
 //require("typeface-inter");
+import { useRouter } from "next/router";
 
 export default function Login() {
+  const router = useRouter();
   const [arrTable, setArrTable] = useState({});
   const [name, setName] = useState("");
 
@@ -46,6 +49,13 @@ export default function Login() {
             setPassword(event.target.value);
           }}
         />
+        <FormHelperText
+          variant="standard"
+          label={'margin="none"'}
+          onClick={() => router.push("/register")}
+        >
+          don't have an account? click here to register
+        </FormHelperText>
         <Button type="button" variant="text" onClick={handleSubmit}>
           Submit
         </Button>
